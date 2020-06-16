5fab3cf63c98397fa8447037812175a8df84290e
EvgenyChernov <evgenycher89@gmail.com>
2020-06-16 13:49:58 +0300

    задание по первому уроку выполнено

---
+++ Урок 1/Задание 4/js/javascript.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let mode = "normal"; //корректно 
+let my_valu3 = 102; //корректно 
+let 3my_value3 = "102"; //некорректно 
+let __hello__ = "world"; //корректно 
+let $$$ = "money"; //корректно 
+let !0_world = true; //некорректно 
