d7b7d6479238ba55e56eb4190d90ec1598d81454
EvgenyChernov <evgenycher89@gmail.com>
2020-06-17 00:04:16 +0300

    задание по первому уроку перепроверено исправлено

--- Урок 1/Задание 1/js/javascript.js
+++ Урок 1/Задание 1/js/javascript.js
@@ -1,4 +1,4 @@
 "use strict";
-let userСelsiusTemperature = parseFloat(prompt("Пожалйста введите температуру в Градусах по Цельсию для конвертации в Градусы Фарйнгейта"));
+let userСelsiusTemperature = Math.fround(prompt("Пожалйста введите температуру в Градусах по Цельсию для конвертации в Градусы Фарйнгейта"));
 let fahrenheitTemperature = (9 / 5) * userСelsiusTemperature + 32;
 alert( userСelsiusTemperature.toFixed(2) + " Градусов Цельсия = " + fahrenheitTemperature.toFixed(2) + " Градусам Фарингейта");
